d897706468e95a6e05a52adb3afd562e946cccb2
Anthony O'Connell <tony@one.ie>
2024-01-29 20:58:14 +0700

    ShadCN UI integrated

---
+++ src/components/examples/accordion.tsx
@@ -0,0 +1,28 @@
+import {
+  Accordion as BaseAccordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger,
+} from "@/components/ui/accordion";
+
+interface Props {
+  data: {
+    title: string;
+    description: string;
+  }[];
+}
+
+export function Accordion({ data }: Props) {
+  return (
+    <BaseAccordion type="single" collapsible className="w-full">
+      {data.map((item, i) => {
+        return (
+          <AccordionItem value={`item-${i.toFixed()}`}>
+            <AccordionTrigger>{item.title}</AccordionTrigger>
+            <AccordionContent>{item.description}</AccordionContent>
+          </AccordionItem>
+        );
+      })}
+    </BaseAccordion>
+  );
+}
